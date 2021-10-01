d7a79b9dd83519d699487d7db81d6a397baa365f
Tommy Bartocci <tommybartocci25@gmail.com>
2021-10-01 15:01:12 -0400

    Initial Commit

---
+++ Encryption/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Encryption
+//
+//  Created by Tommy Bartocci on 10/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
